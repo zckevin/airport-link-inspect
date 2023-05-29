71f675a60cc81087c689710bc867c3ae17a18058
Cong Zuo <zckevinzc@gmail.com>
2023-05-29 12:47:05 +0800

    Add  history to avoid duplicate scapes

--- internal/utils.go
+++ internal/utils.go
@@ -2,9 +2,13 @@ package internal
 
 import (
 	"context"
+	"sync"
+
+	safe "github.com/eminarican/safetypes"
+	"github.com/samber/lo"
 )
 
-func MapAll[KeyT any, ResultT any](
+func MapAllConcurrently[KeyT any, ResultT any](
 	oldctx context.Context,
 	keys []KeyT,
 	callback func(ctx context.Context, key KeyT) (ResultT, error),
@@ -34,6 +38,29 @@ func MapAll[KeyT any, ResultT any](
 	return results, nil
 }
 
+func MapAllConcurrentlyAllSettled[KeyT any, ResultT any](
+	oldctx context.Context,
+	keys []KeyT,
+	callback func(ctx context.Context, key KeyT) (ResultT, error),
+) []safe.Result[ResultT] {
+	ctx, cancel := context.WithCancel(oldctx)
+	defer cancel()
+
+	N := len(keys)
+	results := make([]safe.Result[ResultT], N)
+	var wg sync.WaitGroup
+	wg.Add(N)
+	fn := func(i int, key KeyT) {
+		defer wg.Done()
+		results[i] = safe.AsResult(callback(ctx, key))
+	}
+	for index, ip := range keys {
+		go fn(index, ip)
+	}
+	wg.Wait()
+	return results
+}
+
 func PromiseAll(
 	oldctx context.Context,
 	callbacks []func(ctx context.Context) error,
@@ -57,4 +84,14 @@ func PromiseAll(
 		}
 	}
 	return nil
+}
+
+func UnwrapAll[T any](results []safe.Result[*T]) []*T {
+	return lo.Map(
+		lo.Filter(results, func(result safe.Result[*T], _ int) bool {
+			return result.IsOk() && (result.Unwrap() != nil)
+		}),
+		func(result safe.Result[*T], _ int) *T {
+			return result.Unwrap()
+		})
 }
--- scraper.go
+++ scraper.go
@@ -4,20 +4,49 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"sync"
 
+	"github.com/pkg/errors"
 	"github.com/zckevin/tcp-link-inspect/internal"
 	"github.com/zckevin/tcp-link-inspect/scrapers"
 )
 
-func parseAccessPoint(resolverName string, link *LinkConfig) ([]IPMetaInfo, error) {
-	server, ok := link.RawInfo["server"].(string)
-	if !ok {
-		return nil, fmt.Errorf("no server found in yaml config")
+type ipslice []net.IP
+
+func (ips ipslice) String() string {
+	var key string
+	for _, ip := range ips {
+		key += ip.String()
 	}
-	ips, err := net.LookupIP(server)
-	if err != nil {
-		return nil, err
+	return key
+}
+
+func (ips ipslice) isBlocked() bool {
+	for _, ip := range ips {
+		if ip.IsPrivate() || ip.IsLoopback() || ip.IsLinkLocalUnicast() || ip.IsLinkLocalMulticast() {
+			return true
+		}
 	}
+	return false
+}
+
+var (
+	mu      sync.Mutex
+	history map[string]struct{} = make(map[string]struct{})
+)
+
+func upsetHistory(ips ipslice) bool {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if _, ok := history[ips.String()]; ok {
+		return false
+	}
+	history[ips.String()] = struct{}{}
+	return true
+}
+
+func parseAccessPoint(resolverName string, link *LinkConfig, ips []net.IP) ([]IPMetaInfo, error) {
 	callback := func(ctx context.Context, ip net.IP) (IPMetaInfo, error) {
 		switch resolverName {
 		case "ipinfo":
@@ -28,45 +57,72 @@ func parseAccessPoint(resolverName string, link *LinkConfig) ([]IPMetaInfo, erro
 			panic("unknown resolver name")
 		}
 	}
-	return internal.MapAll(context.Background(), ips, callback)
+	return internal.MapAllConcurrently(context.Background(), ips, callback)
 }
 
 func parseLandingPoint(link *LinkConfig) (IPMetaInfo, error) {
 	return scrapers.ScrapeMetainfoFromCloudflare(context.Background(), link.Proxy)
 }
 
-func ScrapeLinkInfo(link *LinkConfig) (*LinkInfo, error) {
+func ScrapeLinkInfo(link *LinkConfig) (_ *LinkInfo, err error) {
+	logErrorFn := func(err error) {
+		internal.Logger.Err(err).
+			Str("proxy", link.Proxy.Addr()).
+			Str("desc", link.Desc).
+			Msg("")
+	}
+	defer func() {
+		if err != nil {
+			logErrorFn(err)
+		}
+	}()
+
+	server, ok := link.RawInfo["server"].(string)
+	if !ok {
+		return nil, fmt.Errorf("no server found in yaml config")
+	}
+	ips, err := net.LookupIP(server)
+	if err != nil {
+		return nil, err
+	}
+	// if local ip, skip
+	if ipslice(ips).isBlocked() {
+		return nil, fmt.Errorf("non-public ip")
+	}
+	// if access point ip(s) are scraped before, skip the scraping process
+	if appended := upsetHistory(ips); !appended {
+		return nil, nil
+	}
+
 	result := &LinkInfo{
-		Desc: link.Desc,
+		Desc:      link.Desc,
+		ProxyInfo: link.RawInfo,
 	}
 	accessPointFn := func(ctx context.Context) error {
-		infos, err := parseAccessPoint("ipinfo", link)
+		infos, err := parseAccessPoint("ipinfo", link, ips)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "parse access point failed")
 		}
-		// fmt.Printf("%+v\n", infos[0])
 		result.AccessPoint = infos
 		return nil
 	}
 	landingPointFn := func(ctx context.Context) error {
 		lp, err := parseLandingPoint(link)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "parse landing point failed")
 		}
-		// fmt.Printf("%+v\n", lp)
 		result.LandingPoint = lp
 		return nil
 	}
 	rttFn := func(ctx context.Context) error {
-		minRtt, err := internal.ProbeLinkMinRtt(1, link.Proxy)
+		minRtt, err := internal.ProbeLinkMinRtt(2, link.Proxy)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "probe link min rtt failed")
 		}
-		// fmt.Printf("%+v\n", minRtt)
 		result.MinRtt = minRtt
 		return nil
 	}
-	err := internal.PromiseAll(context.Background(), []func(ctx context.Context) error{
+	err = internal.PromiseAll(context.Background(), []func(ctx context.Context) error{
 		accessPointFn,
 		landingPointFn,
 		rttFn,
