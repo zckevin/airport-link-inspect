523bc2278b6dc3ba27b70281a0760a726cca7e42
Cong Zuo <zckevinzc@gmail.com>
2023-05-29 12:44:52 +0800

    Use HTTPS instead of HTTP for rtt probe

--- internal/rtt.go
+++ internal/rtt.go
@@ -10,42 +10,48 @@ import (
 )
 
 const (
-	GOOGLE_RTT_PROBE_URL = "http://www.gstatic.com/generate_204"
+	// uses HTTPS incase some services cheat with HTTP packets inspection and early return without proxying
+	GOOGLE_RTT_PROBE_URL = "https://www.gstatic.com/generate_204"
 )
 
-func probeGoogle(p constant.Proxy) (int, error) {
+func ProbeLinkMinRtt(N int, p constant.Proxy) (int, error) {
 	targetUrlString := GOOGLE_RTT_PROBE_URL
+	Logger.Debug().
+		Str("proxy", p.Addr()).
+		Str("desc", p.Name()).
+		Str("url", targetUrlString).
+		Msg("probe_rtt")
+
 	conn, err := dialProxyConn(context.Background(), p, targetUrlString)
 	if err != nil {
 		return 0, err
 	}
 	defer conn.Close()
 
-	req, err := http.NewRequest(http.MethodHead, targetUrlString, nil)
-	if err != nil {
-		return 0, err
-	}
 	client := createHTTPClient(conn)
 	defer client.CloseIdleConnections()
 
-	start := time.Now()
-	resp, err := client.Do(req)
-	if err != nil {
-		return 0, err
+	fn := func() (int, error) {
+		req, err := http.NewRequest(http.MethodHead, targetUrlString, nil)
+		if err != nil {
+			return 0, err
+		}
+		start := time.Now()
+		resp, err := client.Do(req)
+		if err != nil {
+			return 0, err
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusNoContent {
+			err = fmt.Errorf("http status code: %d", resp.StatusCode)
+			return 0, err
+		}
+		return int(time.Since(start).Milliseconds()), nil
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusNoContent {
-		err = fmt.Errorf("http status code: %d", resp.StatusCode)
-		return 0, err
-	}
-	// fmt.Println(time.Since(start).Milliseconds())
-	return int(time.Since(start).Milliseconds()), nil
-}
 
-func ProbeLinkMinRtt(N int, p constant.Proxy) (int, error) {
 	minRtt := 0
 	for i := 0; i < N; i++ {
-		if rtt, err := probeGoogle(p); err != nil {
+		if rtt, err := fn(); err != nil {
 			return 0, err
 		} else {
 			if minRtt == 0 || rtt < minRtt {
